c6c716921c452446c51a2c0fb84e5d41d6b6786f
jakuzzibubbles <jakuzzi263@gmail.com>
2024-10-11 17:00:25 +0200

    CardComponents

--- frontend/travel-story-app/src/pages/Home/Home.jsx
+++ frontend/travel-story-app/src/pages/Home/Home.jsx
@@ -2,11 +2,16 @@ import { useEffect, useState } from 'react';
 import Navbar from '../../components/Navbar';
 import { useNavigate } from 'react-router-dom';
 import axiosInstance from '../../utils/axiosInstance';
+import TravelStoryCard from '../../components/Cards/TravelStoryCard';
+
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Home = () => {
   const navigate = useNavigate();
 
   const [userInfo, setUserInfo] = useState(null);
+  const [allStories, setAllStories] = useState([]);
 
   // Get User Info
   const getUserInfo = async () => {
@@ -26,7 +31,46 @@ const Home = () => {
     }
   };
 
+  // Get all travel stories
+  const getAllTravelStories = async () => {
+    try {
+      const response = await axiosInstance.get("/get-all-stories");
+      if (response.data && response.data.stories) {
+        setAllStories(response.data.stories);
+      }
+    } catch (error) {
+      console.log("An unexpected error occurred. Please try again.");
+    }
+  };
+
+  // Handle Edit Story Click
+  const handleEdit = (data) => {}
+
+  // Handle Travel Story Click
+  const handleViewStory = (data) => {}
+
+  // Handle Update Favourite
+  const updateIsFavourite = async (storyData) => {
+    const storyId = storyData._id;
+
+    try {
+      const response = await axiosInstance.put("/update-is-favourite/" + storyId,
+        {
+          isFavourite: !storyData.isFavourite,
+        }
+      );
+
+      if (response.data && response.data.story) {
+        toast.success("Story Updated Successfully");
+        getAllTravelStories();
+      }
+    } catch (error) {
+      console.log("An unexpected error occurred. Please try again.");
+    }
+  };
+
   useEffect(() => {
+    getAllTravelStories();
     getUserInfo();
 
     return () => {};
@@ -36,6 +80,38 @@ const Home = () => {
     <>
       <Navbar userInfo={userInfo} />
 
+      <div className='container mx-auto py-10'>
+        <div className='flex gap-7'>
+          <div className='flex-1'>
+            {allStories.length > 0 ? (
+              <div className='grid grid-cols-2 gap4'>
+                {allStories.map((item) => {
+                  return (
+                    <TravelStoryCard
+                    key={item._id}
+                    imgUrl={item.imageUrl}
+                    title={item.title}
+                    story={item.story}
+                    date={item.visitedDate}
+                    visitedLocation={item.visitedLocation}
+                    isFavourite={item.isFavourite}
+                    onEdit={() => handleEdit(item)}
+                    onClick={() => handleViewStory(item)}
+                    onFavouriteClick={() => updateIsFavourite(item)}
+                    />
+                  );
+                })}
+                </div>
+            ) : (
+              <>Empty Card here</>
+            )}
+          </div>
+
+          <div className='w-[320px]'></div>
+        </div>
+      </div>
+
+      <ToastContainer />
     </>
   );
 };
