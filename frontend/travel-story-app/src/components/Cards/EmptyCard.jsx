6fa41cdc0a5592c62289427050f7dcb8e98179b9
jakuzzibubbles <jakuzzi263@gmail.com>
2024-10-14 01:12:27 +0200

    EmptyCard component

--- frontend/travel-story-app/src/components/Cards/EmptyCard.jsx
+++ frontend/travel-story-app/src/components/Cards/EmptyCard.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
-
-const EmptyCard = () => {
+const EmptyCard = ({ imgSrc, message }) => {
   return (
-    <div>
+    <div className='flex flex-col items-center justify-center mt-20'>
+        <img src={imgSrc} alt="No notes" className='w-24' />
+
+        <p className='w-1/2 text-sm font-medium text-slate-700 text-center leading-7 mt-5'>
+        {message}
+        </p>
 
     </div>
   )
