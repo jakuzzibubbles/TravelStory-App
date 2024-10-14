fc12e9490e6b4a39b06198a884efd32afdf6bb0f
jakuzzibubbles <jakuzzi263@gmail.com>
2024-10-14 03:43:19 +0200

    getEmptyCard+Message

--- frontend/travel-story-app/src/utils/helper.js
+++ frontend/travel-story-app/src/utils/helper.js
@@ -1,3 +1,7 @@
+import ADD_STORY_IMG from '../assets/images/add-story-svg';
+import NO_SEARCH_DATA_IMG from '../assets/images/no-search-data.svg';
+import NO_FILTER_IMG from '../assets/images/no-filter-data.svg';
+
 export const validateEmail = (email) => {
     const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return regex.test(email);
@@ -14,4 +18,30 @@ export const getInitials = (name) => {
     }
 
     return initials.toUpperCase();
+};
+
+export const getEmptyCardMessage = (filterType) => {
+    switch (filterType) {
+        case "search":
+            return `Oops! No stories found matching your search.`;
+
+        case "date":
+            return `No stories found in the given date range.`;
+
+        default:
+            return `Start creating your first Travel Story by clicking the "Add" button to jot down your memories and ideas. Let's get started!`;
+    }
+};
+
+export const getEmptyCardImg = (filterType) => {
+    switch (filterType) {
+        case "search":
+        return NO_SEARCH_DATA_IMG;
+
+        case "date":
+        return NO_FILTER_IMG;
+
+        default:
+            return ADD_STORY_IMG;
+    }
 };
