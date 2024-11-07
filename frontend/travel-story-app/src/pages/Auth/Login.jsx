9b4b9f4b6b7f595ac5faba6591ee3cf65f17af1e
jakuzzibubbles <jakuzzi263@gmail.com>
2024-11-07 19:07:24 +0100

    uodated Dockefile, fixed typo in components, refactor main.jsx

--- frontend/travel-story-app/src/pages/Auth/Login.jsx
+++ frontend/travel-story-app/src/pages/Auth/Login.jsx
@@ -1,8 +1,9 @@
-import { useState } from 'react';
-import PasswordInput from '../../components/Input/PasswordInput';
-import { useNavigate } from 'react-router-dom';
-import { validateEmail } from '../../utils/helper';
-import axiosInstance from '../../utils/axiosInstance';
+import React from "react";
+import { useState } from "react";
+import PasswordInput from "../../components/Input/PasswordInput";
+import { useNavigate } from "react-router-dom";
+import { validateEmail } from "../../utils/helper";
+import axiosInstance from "../../utils/axiosInstance";
 
 const Login = () => {
   const [email, setEmail] = useState("");
@@ -54,9 +55,8 @@ const Login = () => {
 
   return (
     <div className="h-screen bg-cyan-100 overflow-hidden relative">
-
-      <div className='login-ui-box right-10 -top-40' />
-      <div className='login-ui-box bg-cyan-300 -bottom-40 right-1/2' />
+      <div className="login-ui-box right-10 -top-40" />
+      <div className="login-ui-box bg-cyan-300 -bottom-40 right-1/2" />
 
       <div className="container h-screen flex items-center justify-center px-20 mx-auto">
         <div className="w-2/4 h-[90vh] flex items-end bg-login-bg-img bg-cover bg-center rounded-lg p-10 z-50">
@@ -65,44 +65,49 @@ const Login = () => {
               Capture Your <br /> Journeys
             </h4>
             <p className="text-[15px] text-black leading-6 pr-7 mt-4">
-              Record your travel experiences and memories in your personal travel journal.
+              Record your travel experiences and memories in your personal
+              travel journal.
             </p>
           </div>
-         </div>
+        </div>
 
-          <div className="w-2/4 h-[75vh] bg-white rounded-r-lg relative p-16 shadow-lg shadow-cyan-200/20">
-            <form onSubmit={handleLogin}>
-              <h4 className="text-2xl font-semibold mb-7">Login</h4>
+        <div className="w-2/4 h-[75vh] bg-white rounded-r-lg relative p-16 shadow-lg shadow-cyan-200/20">
+          <form onSubmit={handleLogin}>
+            <h4 className="text-2xl font-semibold mb-7">Login</h4>
 
-              <input
+            <input
               type="text"
               placeholder="Email"
               className="input-box"
               value={email}
-              onChange={({target}) => {
+              onChange={({ target }) => {
                 setEmail(target.value);
-               }}
-              />
+              }}
+            />
 
-              <PasswordInput value={password}
-              onChange={({target}) => {
+            <PasswordInput
+              value={password}
+              onChange={({ target }) => {
                 setPassword(target.value);
-               }}
-              />
+              }}
+            />
 
-              {error && <p className='text-red-500 text-xs pb-1'>{error}</p>}
+            {error && <p className="text-red-500 text-xs pb-1">{error}</p>}
 
-              <button type="submit" className="btn-primary">
-                LOGIN
-              </button>
+            <button type="submit" className="btn-primary">
+              LOGIN
+            </button>
 
-              <p className="text-xs text-slate-500 text-center my-4">Or</p>
+            <p className="text-xs text-slate-500 text-center my-4">Or</p>
 
-              <button type="submit" className="btn-primary btn-light" onClick={() => {
+            <button
+              type="submit"
+              className="btn-primary btn-light"
+              onClick={() => {
                 navigate("/signup");
               }}
-              >
-                CREATE ACCOUNT
+            >
+              CREATE ACCOUNT
             </button>
           </form>
         </div>
